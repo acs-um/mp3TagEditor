a6f512fac7d3d347a45c46a41f636b59a94c2e7c
Matias <mati.kbza22@gmail.com>
2019-04-22 22:17:22 -0300

    Agrega de a carpetas

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
 from Source.mainWindows import Ui_MainWindow
 import os
+from os import listdir
 
 
 class ListFile:
@@ -16,13 +17,31 @@ class MainWindows(QMainWindow, Ui_MainWindow):
         QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
         self.btnAddListMenu.triggered.connect(self.addToList)
-        self.btnAdd.clicked.connect(self.addToList)
+        self.btnAdd.setText("Add Dir")
+        self.btnAdd.clicked.connect(self.addFolderToList)
 
     def addToList(self):
         path = QFileDialog.getOpenFileName(self, "Choose mp3 file", os.path.expanduser('~/Música'))
         list_file = ListFile(path[0])
-        print(list_file.name)
+        print(list_file.path)
         self.listWidget.addItem(list_file.name)
+
+    def removeFromList(self):
+        pass
+
+    # Agrega los nombres y los paths pero medio hardcodeado
+    def addFolderToList(self):
+        folder = QFileDialog.getExistingDirectory(self, "Choose a folder", os.path.expanduser('~/Música/DOOM OST'))
+        folder_list = listdir(folder)
+        for file in folder_list:
+            path = folder + "/" + file
+            list_file = ListFile(path)
+            print(list_file.name)
+            print(list_file.path)
+            self.listWidget.addItem(list_file.name)
+
+    def playFromPlaylist(self):
+        pass
 
 
 if __name__ == "__main__":
