d6051c9da89f585e364a16f5b3e7387c14a40864
Matias <mati.kbza22@gmail.com>
2019-04-22 21:07:07 -0300

    Agrega temas a la lista y muestra el nombre

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
 from Source.mainWindows import Ui_MainWindow
+import os
+
+
+class ListFile:
+
+    def __init__(self, path):
+        self.path = path
+        self.name = path.split("/")[-1:][0]
 
 
 class MainWindows(QMainWindow, Ui_MainWindow):
@@ -7,6 +15,14 @@ class MainWindows(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):  # Constructor de la clase
         QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
+        self.btnAddListMenu.triggered.connect(self.addToList)
+        self.btnAdd.clicked.connect(self.addToList)
+
+    def addToList(self):
+        path = QFileDialog.getOpenFileName(self, "Choose mp3 file", os.path.expanduser('~/Música'))
+        list_file = ListFile(path[0])
+        print(list_file.name)
+        self.listWidget.addItem(list_file.name)
 
 
 if __name__ == "__main__":
