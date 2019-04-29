835a96e7eb9872a3da22e7479a8b8fc74fe008f9
Matias <mati.kbza22@gmail.com>
2019-04-29 18:11:36 -0300

    TableWidget muestra nombres

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
+from PyQt5.QtCore import QVariant, Qt, QAbstractTableModel
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QHeaderView
 from Source.mainWindows import Ui_MainWindow
 import os
-import vlc
-from os import listdir
+
+
+class ListFileModel(QAbstractTableModel):
+    def __init__(self, items, parent=None):
+        super().__init__(parent)
+        self.items = items
+        self.refresh()
+
+    def refresh(self):
+        self.layoutAboutToBeChanged.emit()
+        self.layoutChanged.emit()
+        self.modelReset.emit()
+
+    def rowCount(self, parent=None, *args, **kwargs):
+        return len(self.items)
+
+    def columnCount(self, parent=None, *args, **kwargs):
+        return 1
+
+    def data(self, QModelIndex, role=None):
+        if not QModelIndex.isValid():
+            return QVariant()
+
+        if role != Qt.DisplayRole:
+            return QVariant()
+
+        data = self.items[QModelIndex.row()].name
+        return data
+
+    def headerData(self, p_int, Qt_Orientation, role=None):
+        return QVariant('Nombre')
 
 
 class ListFile:
@@ -16,71 +46,50 @@ class MainWindows(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):  # Constructor de la clase
         QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
-        self.btnAddListMenu.triggered.connect(self.addToList)
+        self.itemsList = []
+        self.listModel = ListFileModel(self.itemsList, parent=self)
+        self.tableView.setModel(self.listModel)
+        self.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.btnAddListMenu.triggered.connect(self.add_to_list)
         self.btnAdd.setText("Add Dir")
-        self.btnAdd.clicked.connect(self.addFolderToList)
-        # doble click (itemActivated)
-        self.listWidget.itemActivated.connect(self.playFromPlaylist)
+
+        self.btnAdd.clicked.connect(self.add_folder_to_list)
+        # double click (itemActivated)
+        # self.listView.itemActivated.connect(self.play_from_playlist)
         # itemClicked es con un solo click
-        # self.listWidget.itemClicked.connect(self.stopSong)
-        # self.btnPlay.clicked.connect(self.playSong)
-        self.btnStop.clicked.connect(self.stopSong)
-        self.btnRandom.clicked.connect(self.sortList)
+        # self.listWidget.itemClicked.connect(self.stop_song)
 
-    def playSong(self):
-      # for song in playlist:
-        song = '/home/matias/Música/DOOM OST/Mick Gordon - DOOM ' \
-               '(Original Game Soundtrack) (2016) pt. 1/02. Rip & Tear.mp3'
-        player = vlc.MediaPlayer(song)
-        player.play()
-
-    def stopSong(self):
-        pass
-
-    def showItem(self):
-        row = self.listWidget.currentRow()
-        print(self.listWidget.item(row).text())
-
-    def addToList(self):
+    def add_to_list(self):
         path = QFileDialog.getOpenFileName(self, "Choose mp3 file", os.path.expanduser('~/Música/Doom OST'),
                                            "All Files (*);;mp3 Files (*.mp3)")
-        # if path[0].split(".")[-1] == "mp3":
         if path[0].endswith('mp3'):
             list_file = ListFile(path[0])
-            self.listWidget.addItem(list_file.path)
+            self.listModel.items.append(list_file)
+            self.listModel.refresh()
         else:
             # Crear un cartel que no deje agregar archivos no mp3
             print("Archivo no mp3")
 
-    def removeFromList(self):
+    def remove_from_list(self):
         self.listWidget.takeItem(self.listWidget.currentRow())
 
-    # Agrega los nombres y los paths pero medio hardcodeado
-    def addFolderToList(self):
-        folder = QFileDialog.getExistingDirectory(self, "Choose a folder", os.path.expanduser('~/Música/DOOM OST'))
+    def add_folder_to_list(self):
+        folder = QFileDialog.getExistingDirectory(self, "Choose folder", os.path.expanduser('~/Música/DOOM OST'))
         if folder:
-            folder_list = listdir(folder)
+            folder_list = os.listdir(folder)
+            items = []
             for file in folder_list:
                 if file.endswith('mp3'):
-                    path = folder + "/" + file
+                    path = os.path.join(folder, file)
                     list_file = ListFile(path)
-                    self.listWidget.addItem(list_file.path)
-        self.listWidget.sortItems()
+                    items.append(list_file)
+            self.listModel.items.extend(items)
+            self.listModel.refresh()
 
-    def playFromPlaylist(self):
+    def play_from_playlist(self):
         row = self.listWidget.currentRow()
-        song = self.listWidget.item(row).text()
-        player = vlc.MediaPlayer(song)
-        player.play()
-        return player
-
-    def selectedItem(self):
-        for i in range(self.listWidget.count()):
-            list_file = ListFile(self.listWidget.item(i).text())
-            print(list_file.name)
-
-    def sortList(self):
-        self.listWidget.sortItems()
+        file = self.listWidget.item(row).text()
+        # play_pause(file)
 
 
 if __name__ == "__main__":
@@ -88,7 +97,3 @@ if __name__ == "__main__":
     windows = MainWindows()
     windows.show()
     app.exec_()
-
-# main = MainWindows()
-# list_file = main.addToList
-# print(list_file.name)
