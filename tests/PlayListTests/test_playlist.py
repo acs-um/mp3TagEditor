8bc07d40ef993bf478bf9bc985cfcc653f7a4950
Matias <mati.kbza22@gmail.com>
2019-06-25 02:00:21 -0300

    Arreglados tests de playlist

--- tests/PlayListTests/test_playlist.py
+++ tests/PlayListTests/test_playlist.py
@@ -7,7 +7,7 @@ def test_add_file(qtbot):
     widget = MainWindows()
     qtbot.addWidget(widget)
     path = [os.path.join(os.path.abspath("."), "tests/PlayListTests/25 - Bloodborne.mp3")]
-    widget.add_to_list(path)
+    widget.playList.add_to_list(path)
     assert len(widget.listModel.items) == 1
     assert widget.listModel.items[0].name == "25 - Bloodborne.mp3"
 
@@ -16,5 +16,5 @@ def test_add_folder(qtbot):
     widget = MainWindows()
     qtbot.addWidget(widget)
     folder = os.path.join(os.path.abspath("."), "tests/PlayListTests")
-    widget.add_folder_to_list(folder)
+    widget.playList.add_folder_to_list(folder)
     assert widget.listModel.rowCount() == 1
